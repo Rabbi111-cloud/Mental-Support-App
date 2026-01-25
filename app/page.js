09141bfa4a1e4195ff33b181a07adf9e591f11a6
Rabbi111-cloud <diggingdeep0002@gmail.com>
2026-01-26 00:41:49 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-  import Link from "next/link"
+import Link from "next/link"
 
 export default function Home() {
   return (
@@ -22,4 +22,3 @@ export default function Home() {
     </main>
   )
 }
-
